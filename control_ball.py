c02c1ec6dd9fc768523b1a17a0651fcef5a8488a
MSI\adsid <a.d.siddhaarthan@gmail.com>
2021-07-10 17:47:00 -0700

    added gravity

--- control_ball.py
+++ control_ball.py
@@ -7,7 +7,7 @@ windowSurface = pygame.display.set_mode((500, 400), 0, 32)
 pygame.display.set_caption("Bounce")
 
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+WHITE = (220, 220, 220)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
@@ -15,22 +15,25 @@ BLUE = (0, 0, 255)
 info = pygame.display.Info()
 sw = info.current_w
 sh = info.current_h
-y= 0
-yy= sh
+
+radius = 10
+velocity = 0
+acceleration = 9.8
+y= radius
+down_direction_flag = True
+del_t = 0.005
 
 while True:
-    windowSurface.fill(BLACK)
-    pygame.draw.circle(windowSurface, GREEN , (250,y), 13, 0)
-    sleep(.006)
-    y+= 1
-    if y>sh:
-        pygame.draw.circle(windowSurface, GREEN , (250,yy), 13, 0)
-        sleep(.0001)
-        yy +=-1
-    if yy<-.00001:
-        y=0
-        y+= 1
-        pygame.draw.circle(windowSurface, GREEN , (250,y), 13, 0)
+    windowSurface.fill(WHITE)
+    pygame.draw.circle(windowSurface, BLACK , (250,y), radius, 0)
+##    sleep(.006)
+
+    y = y + (velocity * del_t) + (acceleration * del_t**2)/2
+    velocity = velocity + (acceleration * del_t)
+    
+##    print(velocity)
+    if (y > sh - radius) or (y < radius):
+        velocity = - velocity
 
     pygame.display.update()
 
