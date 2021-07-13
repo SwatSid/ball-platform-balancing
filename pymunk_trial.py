3cdf138eef434f29b130cd3c1d854653a4c2a8ee
MSI\adsid <a.d.siddhaarthan@gmail.com>
2021-07-12 19:38:46 -0700

    added __debug__ functionality to run without rendering visuals

--- pymunk_trial.py
+++ pymunk_trial.py
@@ -38,93 +38,93 @@ def spawn_ball(mass, radius, position, friction):
     return body, shape
     
 def main():
-
-    pygame.init()
+    
+    if __debug__:
+        pygame.init()
     screen_width = 900
     screen_height = 600
-    screen = pygame.display.set_mode((screen_width, screen_height))
-    clock = pygame.time.Clock()
+    if __debug__:
+        screen = pygame.display.set_mode((screen_width, screen_height))
+        clock = pygame.time.Clock()
     running = True
 
     ### Physics stuff
     space = pymunk.Space()
     space.gravity = (0.0, -900.0)
-    draw_options = pymunk.pygame_util.DrawOptions(screen)
-    # disable the build in debug draw of collision point since we use our own code.
-    draw_options.flags = (
-        draw_options.flags ^ pymunk.pygame_util.DrawOptions.DRAW_COLLISION_POINTS
-    )
-    ## Balls
-    balls = []
+    if __debug__:
+        draw_options = pymunk.pygame_util.DrawOptions(screen)
+        #disable the build in debug draw of collision point since we use our own code.
+        draw_options.flags = (
+            draw_options.flags ^ pymunk.pygame_util.DrawOptions.DRAW_COLLISION_POINTS
+        )
 
     ### control platform
+    platform_length = 400
     control_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
     control_body.position = screen_width/2, screen_height/2
-    
-    # print(control_body.position)
-    
     control_shape = pymunk.Segment(control_body, \
-            (-200, 0), \
-            (200, 0), 0.0)
+            (-platform_length/2, 0), \
+            (+platform_length/2, 0), 0.0)
     
     space.add(control_body, control_shape)
     
+    if __debug__:
+        ch = space.add_collision_handler(0, 0)
+        ch.data["surface"] = screen
+        ch.post_solve = draw_collision
 
-    ch = space.add_collision_handler(0, 0)
-    ch.data["surface"] = screen
-    ch.post_solve = draw_collision
-
-    mass = 0.1
+    mass = 1
     radius = 25
     position_x = random.randint(screen_width/2 - 200, screen_width/2 + 200)
     position_y = screen_height/2 + radius + 1
     position = position_x, position_y # Tuple of x, y | x is a random number ranging between values to randomize its initial spawn location
     friction = 0.5
     
-    body, shape = spawn_ball(mass, radius, position, friction)
+    ball_body, ball_shape = spawn_ball(mass, radius, position, friction)
     
     
-    space.add(body, shape)
-    balls.append(shape)
+    space.add(ball_body, ball_shape)
     
     
+    turn_rate = 0.01
     
     
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                running = False
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
-                pygame.image.save(screen, "contact_with_friction.png")
+        if __debug__:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    running = False
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+                    pygame.image.save(screen, "contact_with_friction.png")
+               
+            keys = pygame.key.get_pressed()
+            
+            if keys[pygame.K_LEFT]:
+                control_body.angle += turn_rate
+            if keys[pygame.K_RIGHT]:
+                control_body.angle -= turn_rate
+                
+            ## Clear screen
+            screen.fill(pygame.Color("white"))
 
-
-        ### Clear screen
-        screen.fill(pygame.Color("white"))
-
-        ### Draw stuff
-        space.debug_draw(draw_options)
+            ## Draw stuff
+            space.debug_draw(draw_options)
         
-        # control_body.angle = control_body.angle + 0.01
-        
-        balls_to_remove = []
-        for ball in balls:
-            if ball.body.position.y < 200:
-                balls_to_remove.append(ball)
-        for ball in balls_to_remove:
-            space.remove(ball, ball.body)
-            balls.remove(ball)
+        if ball_body.position.y < 0:
+            running = False
 
         ### Update physics
         dt = 1.0 / 60.0
         for x in range(1):
             space.step(dt)
 
-        ### Flip screen
-        pygame.display.flip()
-        clock.tick(50)
-        pygame.display.set_caption("fps: " + str(clock.get_fps()))
+        if __debug__:
+            ### Flip screen
+            pygame.display.flip()
+            clock.tick(50)
+            pygame.display.set_caption("fps: " + str(clock.get_fps()))
 
 
 if __name__ == "__main__":
